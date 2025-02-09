42435e9e84426b89acdc4f2ba0b366f80deeadd2
Marllon Zucolotto <marlonzucolotto@gmail.com>
2025-02-08 21:56:06 -0300

    Adicionando a função Limpar

--- lib/main.dart
+++ lib/main.dart
@@ -33,12 +33,11 @@ class _MinhaCalculadoraDeImcState extends State<MinhaCalculadoraDeImc> {
   late TextEditingController pesoController;
   late TextEditingController alturaController;
 
-
   double? imc;
   String? classificacao;
   Color? corResultado;
 
-   @override
+  @override
   void initState() {
     pesoController = TextEditingController(text: '');
     alturaController = TextEditingController(text: '');
@@ -104,7 +103,7 @@ class _MinhaCalculadoraDeImcState extends State<MinhaCalculadoraDeImc> {
                   children: [
                     Text('Seu Peso'),
                     SizedBox(height: 6),
-                    Container(
+                    SizedBox(
                       width: 80,
                       child: TextField(
                         controller: pesoController,
@@ -124,7 +123,7 @@ class _MinhaCalculadoraDeImcState extends State<MinhaCalculadoraDeImc> {
                   children: [
                     Text('Sua Altura'),
                     SizedBox(height: 6),
-                    Container(
+                    SizedBox(
                       width: 80,
                       child: TextField(
                         controller: alturaController,
@@ -142,34 +141,44 @@ class _MinhaCalculadoraDeImcState extends State<MinhaCalculadoraDeImc> {
               ],
             ),
             SizedBox(height: 20),
-            Container(
+            SizedBox(
               width: 200,
               height: 60,
               child: ElevatedButton(
                 onPressed: () {
-                  try {
-                    double peso = double.parse(pesoController.text);
-                    double altura = double.parse(alturaController.text);
-                    setState(
-                      () {
-                        imc = peso / (altura * altura);
-                        classificacao = getClassificacaoIMC(imc!);
-                        corResultado = getCorIMC(imc!);
-                      },
-                    );
-                  } on Exception {
-                    imc = null;
-                    classificacao = null;
-                    corResultado = null;
-                    pesoController.text = '';
-                    alturaController.text = '';
+                  if (imc == null) {
+                    try {
+                      double peso = double.parse(pesoController.text);
+                      double altura = double.parse(alturaController.text);
+                      setState(
+                        () {
+                          imc = peso / (altura * altura);
+                          classificacao = getClassificacaoIMC(imc!);
+                          corResultado = getCorIMC(imc!);
+                        },
+                      );
+                    } on Exception {
+                      imc = null;
+                      classificacao = null;
+                      corResultado = null;
+                      pesoController.text = '';
+                      alturaController.text = '';
+                    }
+                  } else {
+                    setState(() {
+                      imc = null;
+                      classificacao = null;
+                      corResultado = null;
+                      pesoController.text = '';
+                      alturaController.text = '';
+                    });
                   }
                 },
                 style: ButtonStyle(
                   backgroundColor: WidgetStateProperty.all(Colors.deepPurple),
                 ),
                 child: Text(
-                  'Calcular',
+                  imc == null ? 'Calcular' : 'Limpar',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
